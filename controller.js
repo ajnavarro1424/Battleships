74048e9d87cf6e4ad11a883c7036b1865f4b64c4
cabelhigh <cabelhigh@yahoo.com>
2017-02-07 10:10:44 -0800

    Implemented simple click on cell functionality

--- controller.js
+++ controller.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-  buildTable();
+  buildTable(); //Builds 100 cell table which is the board
+
+  $("td").on("click", function() {
+    $(this).addClass("miss"); //Adds red .miss class wheneve u click
+  })
 
 });
 
 function buildTable() {
-  var currentRow = 0;
-  for(var i = 0; i <100; i++){
-    if(i%10==0){
-      currentRow = i/10;
-      $("#board").append('<tr id="row' + currentRow + '"></tr>');
-      console.log("We're here!")
+  var currentRow = 0; //variable keeps track of current row
+  for(var i = 0; i <100; i++){ //for loop creates 100 cell table
+    if(i%10==0){ // if counter%10 is 0, make a new row
+      currentRow = i/10; //update current row
+      $("#board").append('<tr id="row' + currentRow + '"></tr>'); //create a new table row with id "row<currentRow>"
     }
-    $("#row"+ currentRow).append('<td id="index-' + i + '"></td>');
+    $("#row"+ currentRow).append('<td id="index-' + i + '"></td>'); //makes a new table cell with id "index<i>" under "row<currentRow"
 
   }
 }
